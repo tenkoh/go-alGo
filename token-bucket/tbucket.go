9327ddcba51497a670136d01422b91fd5886352f
tenkoh <tenkoh.go@gmail.com>
2023-09-13 06:05:11 +0900

    token-bucket: add constructor and get method

---
+++ token-bucket/tbucket.go
@@ -0,0 +1,64 @@
+// Package tbucket is an implementation of a token bucket algorithm.
+package tbucket
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+)
+
+// Bucket is a token bucket.
+// Tokens are added to the bucket at a fixed interval.
+type Bucket struct {
+	ticker *time.Ticker
+	bucket chan struct{}
+}
+
+// NewBucket creates a new token bucket.
+// A ticker is created to add tokens to the bucket at a fixed interval.
+func NewBucket(size int, refillInterval time.Duration) (*Bucket, error) {
+	var err error
+	if size <= 0 {
+		err = errors.New("size must be greater than 0")
+	}
+	if refillInterval <= 0 {
+		err = errors.Join(err, errors.New("refillInterval must be greater than 0"))
+	}
+	if err != nil {
+		return nil, err
+	}
+	b := &Bucket{
+		ticker: time.NewTicker(refillInterval),
+		bucket: make(chan struct{}, size),
+	}
+
+	// fill the bucket
+	for i := 0; i < size; i++ {
+		b.bucket <- struct{}{}
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		wg.Done()
+		for {
+			_, ok := <-b.ticker.C
+			if !ok {
+				close(b.bucket)
+				return
+			}
+			if len(b.bucket) < size {
+				b.bucket <- struct{}{}
+			}
+		}
+	}()
+	wg.Wait()
+
+	return b, nil
+}
+
+// Get returns a channel that will sent a token when one is available.
+func (b *Bucket) Get(ctx context.Context) <-chan struct{} {
+	return b.bucket
+}
--- token-bucket/tbucket_test.go
+++ token-bucket/tbucket_test.go
@@ -1,7 +1,47 @@
 package tbucket_test
 
+import (
+	"context"
+	"tbucket"
+	"testing"
+	"time"
+)
+
 // 1. bucketに空きがある場合はtokenを取り出せる
 // 2. bucketに空きがない場合はtokenを取り出せない(ブロックされる)
 // 3. bucketからの取り出しはキャンセルできる
 // 4. bucketには決めた間隔でtokenが補充される
 // 5. bucketには最大値を超えてtokenを補充できない
+// 6. bucketの最大サイズは1以上でなければならない
+// 7. bucketの補充間隔は0より大きくなければならない
+
+func TestBucket_Get(t *testing.T) {
+	bucketSize := 3
+	interval := 500 * time.Millisecond
+	tolerance := interval / 10
+
+	ctx := context.Background()
+
+	b, err := tbucket.NewBucket(bucketSize, interval)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// Confirm that the bucket is not blocked
+	t0 := time.Now()
+	for i := 0; i < bucketSize; i++ {
+		<-b.Get(ctx)
+	}
+	t1 := time.Now()
+	if t1.Sub(t0) > tolerance {
+		t.Errorf("bucket is blocked from being taken out")
+	}
+
+	// Confirm that the bucket is blocked
+	<-b.Get(ctx)
+	t1 = time.Now()
+	if dt := t1.Sub(t0); dt > interval+tolerance || dt < interval-tolerance {
+		t.Errorf("bucket is not blocked properly")
+	}
+}
