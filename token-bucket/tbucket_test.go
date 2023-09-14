31bdc378227f44addcd40656e891608d00f7941b
tenkoh <tenkoh.go@gmail.com>
2023-09-14 22:12:03 +0900

    token-bucket: pass bucket-cancel test

--- token-bucket/tbucket_test.go
+++ token-bucket/tbucket_test.go
@@ -9,8 +9,8 @@ import (
 
 // 1. ok bucketに空きがある場合はtokenを取り出せる
 // 2. ok bucketに空きがない場合はtokenを取り出せない(ブロックされる)
-// 3. bucketからの取り出しはキャンセルできる
-// 4. bucketには決めた間隔でtokenが補充される
+// 3. ok bucketからの取り出しはキャンセルできる
+// 4. ok bucketには決めた間隔でtokenが補充される
 // 5. bucketには最大値を超えてtokenを補充できない
 // 6. bucketの最大サイズは1以上でなければならない
 // 7. bucketの補充間隔は0より大きくなければならない
@@ -38,10 +38,17 @@ func TestBucket_Get(t *testing.T) {
 		t.Errorf("bucket is blocked from being taken out")
 	}
 
-	// Confirm that the bucket is blocked
+	// Confirm that the bucket is blocked, and the token is reffiled after the interval
 	b.Get(ctx)
 	t1 = time.Now()
 	if dt := t1.Sub(t0); dt > interval+tolerance || dt < interval-tolerance {
 		t.Errorf("bucket is not blocked properly")
 	}
+
+	// Confirm that the bucket can be canceled
+	ctx1, cancel1 := context.WithTimeout(ctx, interval/10)
+	if err := b.Get(ctx1); err == nil {
+		t.Errorf("bucket is not canceled")
+	}
+	defer cancel1()
 }
